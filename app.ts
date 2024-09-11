c05b34926aed4288b82c507e673a9daf83fdad03
`mayankjoshi` <`mayankjoshi9871@gmail.com`>
2024-09-11 16:56:20 +0530

    updating upload size

--- app.ts
+++ app.ts
@@ -38,8 +38,8 @@ const corsOptions: CorsOptionsDelegate = (req, callback) => {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(fileUpload());
 app.use(expressSession({ 
     secret: process.env.EXPRESS_SESSION_KEY!,
