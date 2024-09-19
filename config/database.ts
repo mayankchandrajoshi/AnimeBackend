5bfc302e459d4b426e4e3c6e87205a7512e7ba4e
`mayankjoshi` <`mayankjoshi9871@gmail.com`>
2024-09-19 23:10:21 +0530

    updated database connection function for serverless environment

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDatabase=async ()=>{
+    if (isConnected) return;
     try {
-        const conn = await mongoose.connect(process.env.DB_URI!);
+        await mongoose.connect(process.env.DB_URI!);
+        isConnected = true;
+        console.log("Database connected");    
     } catch (error) {
         console.log("Cannot connect to database");
     }
